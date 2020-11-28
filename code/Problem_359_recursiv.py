193f1e7f7892c267c47692c75c9372a9d8260b88
Pascal Reinhardt <Pascal.reinhardt@hotmail.com>
2020-11-28 18:02:49 +0100

    added Code from Mentor jenna84#1460 from Discord

--- code/Problem_359_recursiv.py
+++ code/Problem_359_recursiv.py
@@ -1,8 +1,12 @@
+import collections
 import re
 # pip install word2number
+from num2words import num2words as n2w
 from word2number import w2n
 
-numberList = [] 
+numberList = []
+
+
 def removeLettersFromString(Sstring, Sletters):
     try:
         Sstring = str(Sstring)
@@ -19,17 +23,39 @@ def stringContainsAllLetters(string, letters):
     for letter in letters:
         if letter in string:
             counter += 1
-            string = removeLettersFromString(string,letter)
+            string = removeLettersFromString(string, letter)
         else:
             return False
 
     return True
 
-#add constain if angram contains z,w,u,x,g remove (z)ero, t(w)o, fo(u)r, si(x), ei(g)ht because their appearence is unique
+
+def solveAnagramJenna(anagram):
+    frequency = collections.Counter(anagram)
+    solution = []
+    for number, char in (
+            (0, 'z'),
+            (2, 'w'),
+            (4, 'u'),
+            (5, 'f'),
+            (6, 'x'),
+            (7, 's'),
+            (8, 'g'),
+            (9, 'i'),
+            (1, 'o'),
+            (3, 't')
+    ):
+        solution += [number] * frequency[char]
+        frequency -= collections.Counter(n2w(number) * frequency[char])
+    return sorted(solution)
+
+
+# add constain if angram contains z,w,u,x,g remove (z)ero, t(w)o, fo(u)r, si(x), ei(g)ht because their appearence is
+# unique
 def solveAnagram(anagram):
     numberList = []
 
-    if(len(anagram) == 0):
+    if len(anagram) == 0:
         return numberList
     else:
         numbers = ["zero", "one", "two", "three", "four",
@@ -47,11 +73,10 @@ def solveAnagram(anagram):
                 anagram = oldAnagram
                 numberList.pop()
 
-
 # anagram = "noethreesveen"
 # anagram = "oentrheeisxevenseerthinenzero"
 # anagram = "threesevenfoursixtwo"
 # anagram = "fivezerotwo"
 
 # print(solveAnagram("threesevenfoursixtwooneone"))
-# print(solveAnagram(anagram))
+# print(solveAnagramJenna("fourthreeone"))
--- code/test_359_recursiv.py
+++ code/test_359_recursiv.py
@@ -6,28 +6,33 @@ from num2words import num2words as n2w
 
 class Test359_recursiv(unittest.TestCase):
     def test_removeLettersFromString(self):
-        self.assertEqual(Problem_359_recursiv.removeLettersFromString("Test","es"), "Tt")
-        self.assertEqual(Problem_359_recursiv.removeLettersFromString("","Test"),"")
-        self.assertEqual(Problem_359_recursiv.removeLettersFromString("Test",""),"Test")
-        self.assertEqual(Problem_359_recursiv.removeLettersFromString(2323,2323),"")
-        self.assertEqual(Problem_359_recursiv.removeLettersFromString([4,23,4543,],"23"),"[4, , 4543]")
+        self.assertEqual(Problem_359_recursiv.removeLettersFromString("Test", "es"), "Tt")
+        self.assertEqual(Problem_359_recursiv.removeLettersFromString("", "Test"), "")
+        self.assertEqual(Problem_359_recursiv.removeLettersFromString("Test", ""), "Test")
+        self.assertEqual(Problem_359_recursiv.removeLettersFromString(2323, 2323), "")
+        self.assertEqual(Problem_359_recursiv.removeLettersFromString([4, 23, 4543, ], "23"), "[4, , 4543]")
 
     def test_solveAnagram(self):
 
-        for a in range(0,10**2):
+        for a in range(0, 10 ** 2):
             anagram = ''
             number = []
             for x in random.sample(range(0, 9), 3):
-                anagram+=n2w(x)
+                anagram += n2w(x)
                 number.append(x)
             number = sorted(number)
             result = Problem_359_recursiv.solveAnagram(anagram)
-            print(result)
-            self.assertEqual(result,number)
+            self.assertEqual(result, number)
 
-
-
-
+    def test_solveAnagramJenna(self):
+        anagram = ''
+        number = []
+        for x in random.sample(range(0, 9), 3):
+            anagram += n2w(x)
+            number.append(x)
+        number = sorted(number)
+        result = Problem_359_recursiv.solveAnagramJenna(anagram)
+        self.assertEqual(result, number)
 
 
 if __name__ == '__main__':
